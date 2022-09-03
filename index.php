cf89c1327d40313b7ac51d7ef4e0346abf2c98e1
takahara tomoya <t.tkorock221@gmail.com>
2022-09-03 23:00:18 +0900

    セッションまで完了

--- index.php
+++ index.php
@@ -5,6 +5,10 @@
 <body>
 <p>普通のpタグ</p>
 <?php
+    session_name("dot");
+    session_start();
+?>
+<?php
     $num1 = 12;
     $num =& $num1;
     $num1 = 15;
@@ -79,6 +83,7 @@
     class Direction {
         private $azimuth;
 
+        const DEFAULT = "北";
 
         function __construct() {
             $this -> azimuth = "南";
@@ -97,6 +102,50 @@
             print "あなたのいる方角は".$this->azimuth. "です";
         }
     }
+
+    // if (!isset($_SERVER['PHP_AUTH_USER'])){
+    //     header('WWW-Authenticate: Basic realm="Private Page"');
+    //     header('HTTP/1.0 401 Unauthorized');
+
+    //     die('このページを見るにはログインが必要です。');
+    // }
+
+    if(isset($_COOKIE["visited"]) && $_COOKIE["visited"]<10){
+        $cookie = $_COOKIE["visited"];
+        $cookie = $cookie + 3;
+    }else if(isset($_COOKIE["visited"]) && $_COOKIE["visited"]>=10){
+        // 10より大きくはならない
+        setcookie("visited" , 2 , time() + 5);
+    }else {
+        $cookie = 1;
+    }
+
+    print "COOKIEID".$_COOKIE[session_name()]."<br>";
+
+
+    //５秒間操作がなかったcookieの値がリセット
+    $sid = setcookie("visited" , $cookie + 1 , time() + 5);
+
+    print "<br />";
+
+    //これは機能する
+    $_SESSION = array();
+
+    if(!isset($_SESSION["test"])){
+        print ”せっしょんがまだないです”;
+        $_SESSION["test"]=4;
+        print $_SESSION["test"];
+    }else {
+
+        print "セッショんがあります";
+        print $_SESSION["test"];
+    }
+
+    // なんかできない
+    // unset($_SESSION("test"));
+
+    print session_name()."<br>";
+    print session_id();
 
 ?>
 </body>
